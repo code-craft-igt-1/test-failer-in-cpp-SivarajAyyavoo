e3e3e7ecc1e1445f464380239f8d03a057ce8f74
SivarajAyyavoo <41547753+SivarajAyyavoo@users.noreply.github.com>
2024-08-08 10:12:37 +0530

    Introduced folder structure to deal with linters

--- src/tshirts_size_conversion.cpp
+++ src/tshirts_size_conversion.cpp
@@ -1,4 +1,4 @@
-#include "tshirts_size_conversion.h"
+#include "inc/tshirts_size_conversion.h"
 #include <assert.h>
 #include <iostream>
 
@@ -7,13 +7,11 @@ namespace tshirts {
         char sizeName = '\0';
         if (cms < 38) {
             sizeName = 'S';
-        }
-        else if (cms > 38 && cms < 42) {
+        } else if (cms > 38 && cms < 42) {
             sizeName = 'M';
-        }
-        else if (cms > 42) {
+        } else if (cms > 42) {
             sizeName = 'L';
         }
         return sizeName;
     }
-}
+} // namespace tshirts
---
+++ test/tshirts_size_conversion_test.cpp
@@ -0,0 +1,13 @@
+#include "inc/tshirts_size_conversion.h"
+#include "gtest/gtest.h"
+
+namespace tshirts {
+namespace test {
+
+    TEST(TshirtsSizeConversion, CornerCaseTests) {
+        EXPECT_EQ(tshirts::size(38), 'S');
+        EXPECT_EQ(tshirts::size(42), 'M');
+    }
+
+} // namespace test
+} // namespace tshirts
